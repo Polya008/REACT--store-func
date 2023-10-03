814ec14899ac372cda5b0ab32b2ecbcb347efc9c
Polya008 <polskaya007@mail.ru>
2023-10-03 11:24:59 +0300

    version 2

--- src/components/ShopItemFunc.jsx
+++ src/components/ShopItemFunc.jsx
@@ -1,17 +1,19 @@
 export default class ShopItemFunc = () => {
     return (
-        <div class="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div class="description">{item.descriptionFull}</div>
-        <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-        <div class="divider"></div>
-        <div class="purchase-info">
-            <div class="price">{item.currency}{item.price}</div>
-            <button>Добавить в корзину</button>
-        </div>
-        </div>
+        <>
+            <div class="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div class="description">{item.descriptionFull}</div>
+            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+            <div class="divider"></div>
+            <div class="purchase-info">
+                <div class="price">{item.currency}{item.price}</div>
+                <button>Добавить в корзину</button>
+            </div>
+            </div>
+        </>
             )
         }
 
